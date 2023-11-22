7ca5b8c4885a81d4a2eedd7f9ce15d6ed53333cd
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2023-11-22 21:35:50 +0800

    distributed sampling and loading graph

--- python/bifeat/compression/compresser.py
+++ python/bifeat/compression/compresser.py
@@ -1,21 +1,26 @@
 import torch
 import dgl
 import BiFeatLib as capi
+import torch.distributed as dist
+from ..shm import ShmManager
 
 
 class CompressionManager(object):
 
-    def __init__(self, ratios, methods, configs, cache_path):
+    def __init__(self, ratios, methods, configs, cache_path,
+                 shm_manager: ShmManager):
         self.ratios = ratios
         self.methods = methods
         self.configs = configs
         self.cache_path = cache_path
+        self.shm_manager = shm_manager
 
-    def register(self, indptr, indices, features, train_seeds):
+    def register(self, indptr, indices, train_seeds, labels, features):
         self.indptr = indptr
         self.indices = indices
-        self.features = features
         self.train_seeds = train_seeds
+        self.labels = labels
+        self.features = features
 
         self.original_size = self.features.numel(
         ) * self.features.element_size()
@@ -29,56 +34,83 @@ class CompressionManager(object):
         g = dgl.graph(('csc', (self.indptr, self.indices,
                                torch.empty(0, dtype=torch.int64))))
 
+        rank = dist.get_rank()
+        world_size = dist.get_world_size()
+
+        part_size = (self.train_seeds.numel() + world_size - 1) // world_size
+        start = rank * part_size
+        end = (rank + 1) * part_size
+
+        local_train_seeds = self.train_seeds[start:end]
         dataloader = dgl.dataloading.DataLoader(
             g,
-            self.train_seeds,
+            local_train_seeds,
             sampler,
             batch_size=512,
             device='cuda',
             num_workers=0,
             use_uva=True,
+            shuffle=True,
         )
         for input_nodes, output_nodes, blocks in dataloader:
             for block in blocks:
                 self.hotness[block.dstdata[dgl.NID]] += 1
             self.hotness[output_nodes] += 1
 
+        dist.all_reduce(self.hotness, op=dist.ReduceOp.SUM)
+
     def graph_reorder(self):
-        assert self.hotness is not None
+        if self.shm_manager._is_chief:
+            assert self.hotness is not None
 
-        max_hot = self.hotness.max()
-        self.hotness[self.train_seeds] += max_hot
+            max_hot = self.hotness.max()
+            self.hotness[self.train_seeds] += max_hot
 
-        self.hotness, dst2src = torch.sort(self.hotness, descending=True)
-        self.dst2src = dst2src.cpu()
+            self.hotness, dst2src = torch.sort(self.hotness, descending=True)
+            self.dst2src = dst2src.cpu()
 
-        self.src2dst = torch.empty_like(self.dst2src)
-        self.src2dst[self.dst2src] = torch.arange(self.dst2src.numel())
+            self.src2dst = torch.empty_like(self.dst2src)
+            self.src2dst[self.dst2src] = torch.arange(self.dst2src.numel())
 
-        # create
-        new_indptr = torch.zeros_like(self.indptr)
-        new_indices = torch.empty_like(self.indices)
+            # create
+            new_indptr = torch.zeros_like(self.indptr)
+            new_indices = torch.empty_like(self.indices)
 
-        degrees = self.indptr[1:] - self.indptr[:-1]
-        new_indptr[1:] = degrees[self.dst2src].cumsum(dim=0)
+            degrees = self.indptr[1:] - self.indptr[:-1]
+            new_indptr[1:] = degrees[self.dst2src].cumsum(dim=0)
 
-        print(new_indptr)
+            # create new_indices
+            # for i in range(self.indptr.numel() - 1):
+            #    dst = i
+            #    src = self.dst2src[dst].item()
+            #    new_indices[new_indptr[dst].item():new_indptr[dst + 1].item(
+            #    )] = self.indices[self.indptr[src].item():self.indptr[src +
+            #                                                          1].item()]
+            capi.omp_reorder_indices(self.indptr, self.indices, new_indptr,
+                                     new_indices, self.dst2src)
 
-        # create new_indices
-        # for i in range(self.indptr.numel() - 1):
-        #    dst = i
-        #    src = self.dst2src[dst].item()
-        #    new_indices[new_indptr[dst].item():new_indptr[dst + 1].item(
-        #    )] = self.indices[self.indptr[src].item():self.indptr[src +
-        #                                                          1].item()]
-        capi.omp_reorder_indices(self.indptr, self.indices, new_indptr,
-                                 new_indices, self.dst2src)
+            new_indices = self.src2dst[new_indices]
 
-        new_indices = self.src2dst[new_indices]
+            self.indptr[:] = new_indptr
+            self.indices[:] = new_indices
+            self.train_seeds[:] = self.src2dst[self.train_seeds]
+            self.labels[:] = self.labels[self.dst2src]
 
-        self.indptr = new_indptr
-        self.indices = new_indices
-        self.train_seeds = self.src2dst[self.train_seeds]
+            # recovery hotness
+            self.hotness[self.train_seeds] -= max_hot
 
-        # recovery hotness
-        self.hotness[self.train_seeds] -= max_hot
+            # for broadcast
+            dst2src = self.dst2src.cpu()
+            hotness = self.hotness.cpu()
+            package = [dst2src, hotness]
+        else:
+            package = [None, None]
+
+        dist.barrier(self.shm_manager._local_group)
+
+        # brocast hotness and dst2src
+        dist.broadcast_object_list(package,
+                                   0,
+                                   group=self.shm_manager._local_group)
+        self.dst2src = package[0]
+        self.hotness = package[1]
---
+++ test.py
@@ -0,0 +1,65 @@
+import torch
+import bifeat
+import numpy as np
+import os
+from torch import distributed as dist
+import time
+
+
+def main(num_gpus=2):
+    dist.init_process_group(backend='nccl')
+    rank = dist.get_rank()
+    world_size = dist.get_world_size()
+
+    print("{} of {}".format(rank, world_size))
+
+    omp_thread_num = os.cpu_count() // num_gpus
+    torch.cuda.set_device(rank)
+    torch.set_num_threads(omp_thread_num)
+
+    print("Set device to {} and cpu threads num {}".format(
+        rank, omp_thread_num))
+
+    chief = rank % num_gpus == 0
+
+    shm_manager = bifeat.shm.ShmManager(rank,
+                                        num_gpus,
+                                        './datasets',
+                                        'ogbn-products',
+                                        pin_memory=True)
+
+    begin = time.time()
+    graph_tensors, meta_data = shm_manager.load_dataset(with_feature=True)
+    torch.cuda.synchronize()
+    print("Load dataset time: {}".format(time.time() - begin))
+
+    print(graph_tensors['indptr'])
+
+    compression_manager = bifeat.CompressionManager(None, None, None, None,
+                                                    shm_manager)
+    compression_manager.register(
+        graph_tensors['indptr'],
+        graph_tensors['indices'],
+        graph_tensors['train_idx'],
+        graph_tensors['labels'],
+        graph_tensors['features'],
+    )
+
+    begin = time.time()
+    compression_manager.presampling([10, 10])
+    torch.cuda.synchronize()
+    print("presampling time: {}".format(time.time() - begin))
+
+    begin = time.time()
+    compression_manager.graph_reorder()
+    torch.cuda.synchronize()
+    print("graph_reorder time: {}".format(time.time() - begin))
+
+    begin = time.time()
+    compression_manager.compress()
+    torch.cuda.synchronize()
+    print("compress time: {}".format(time.time() - begin))
+
+
+if __name__ == '__main__':
+    main()
