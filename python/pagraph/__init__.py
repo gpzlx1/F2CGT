99a10ee6d99a5f2245d221619a1f855c267b5f5c
CommediaJW <552388389@qq.com>
2023-11-22 12:05:54 +0000

    remove unecessary code

--- python/pagraph/__init__.py
+++
@@ -1,9 +0,0 @@
-import torch
-import os
-
-package_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
-so_path = os.path.join(package_path, 'libpg.so')
-torch.classes.load_library(so_path)
-
-from .feature_cache import FeatureCache
-from .quantization import *
