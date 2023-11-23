ce70096ca58c72b1c80ed71368d3dd811ffb7873
CommediaJW <552388389@qq.com>
2023-11-23 05:26:38 +0000

    new feature cache

--- example/demo.py
+++ example/demo.py
@@ -3,27 +3,78 @@ import time
 from bifeat import FeatureCacheServer
 
 if __name__ == "__main__":
-    cpu_data = torch.arange(0, 10000).reshape(100, 100).float()
-    index = torch.randint(0, 100, (100, )).long().cuda()
+
+    core_data = torch.arange(1000, 2000).reshape(10, 100).float()
+    core_nids = torch.cat([torch.arange(50, 55), torch.arange(95, 100)])
+    other_data = torch.arange(0, 1000).reshape(100, 10).float()
+    other_nids = torch.cat(
+        [torch.arange(0, 50),
+         torch.arange(55, 95),
+         torch.arange(100, 110)])
+    idx_map = torch.zeros((110, )).long()
+    idx_map[core_nids] = torch.arange(0, 10)
+    idx_map[other_nids] = torch.arange(0, 100)
+    core_mask = torch.zeros((110, ), dtype=torch.bool)
+    core_mask[core_nids] = True
+
+    fetch_nids = torch.randperm(110)[:30]
+
+    for _ in range(5):
+        tic = time.time()
+        mask_ = core_mask[fetch_nids]
+        core_ = core_data[idx_map[fetch_nids[mask_]]].cuda()
+        other_ = other_data[idx_map[fetch_nids[~mask_]]].cuda()
+        torch.cuda.synchronize()
+        toc = time.time()
+        print(toc - tic)
 
     # part cache
-    cache_nids = torch.randint(0, 100, (20, )).int().cuda()
-    part_cache_server = FeatureCacheServer(cpu_data)
-    part_cache_server.cache_feature(cache_nids, False)
-    assert part_cache_server.fetch_data(index).equal(
-        cpu_data[index.cpu().long()].cuda())
+    core_cache_idx = torch.randperm(10)[:4]
+    other_cache_idx = torch.randperm(100)[:40]
+    part_cache_server = FeatureCacheServer(core_data, core_nids, other_data,
+                                           other_nids)
+    part_cache_server.cache_data(core_cache_idx, other_cache_idx)
+    for _ in range(5):
+        tic = time.time()
+        core, other, mask = part_cache_server[fetch_nids.cuda()]
+        torch.cuda.synchronize()
+        toc = time.time()
+        print(toc - tic)
+    assert core.equal(core_)
+    assert other.equal(other_)
+    assert mask.equal(mask_.cuda())
     del part_cache_server
 
     # full cache
-    full_cache_server = FeatureCacheServer(cpu_data)
-    full_cache_server.cache_feature(torch.tensor([]).int().cuda(), True)
-    assert full_cache_server.fetch_data(index).equal(
-        cpu_data[index.cpu().long()].cuda())
+    core_cache_idx = torch.randperm(10)[:10]
+    other_cache_idx = torch.randperm(100)[:100]
+    full_cache_server = FeatureCacheServer(core_data, core_nids, other_data,
+                                           other_nids)
+    full_cache_server.cache_data(core_cache_idx, other_cache_idx)
+    for _ in range(5):
+        tic = time.time()
+        core, other, mask = full_cache_server[fetch_nids.cuda()]
+        torch.cuda.synchronize()
+        toc = time.time()
+        print(toc - tic)
+    assert core.equal(core_)
+    assert other.equal(other_)
+    assert mask.equal(mask_.cuda())
     del full_cache_server
 
-    # no cache
-    no_cache_server = FeatureCacheServer(cpu_data)
-    no_cache_server.cache_feature(torch.tensor([]).int().cuda(), False)
-    assert no_cache_server.fetch_data(index).equal(
-        cpu_data[index.cpu().long()].cuda())
-    del no_cache_server
+    # empty cache
+    core_cache_idx = torch.tensor([])
+    other_cache_idx = torch.tensor([])
+    empty_cache_server = FeatureCacheServer(core_data, core_nids, other_data,
+                                            other_nids)
+    empty_cache_server.cache_data(core_cache_idx, other_cache_idx)
+    for _ in range(5):
+        tic = time.time()
+        core, other, mask = empty_cache_server[fetch_nids.cuda()]
+        torch.cuda.synchronize()
+        toc = time.time()
+        print(toc - tic)
+    assert core.equal(core_)
+    assert other.equal(other_)
+    assert mask.equal(mask_.cuda())
+    del empty_cache_server
--- python/bifeat/cache/feature_cache.py
+++ python/bifeat/cache/feature_cache.py
@@ -5,64 +5,139 @@ import BiFeatLib as capi
 
 class FeatureCacheServer:
 
-    def __init__(self, feature):
-        self.feature = feature
-        self.cached_feature = None
-        self.hashmap_key = None
-        self.hashmap_value = None
-        self.full_cached = False
-        self.no_cached = False
+    def __init__(self, core_feature, core_nids, other_feature, other_nids):
+        self._core_feature = core_feature
+        self._other_feature = other_feature
 
-    def cache_feature(self, cache_nids, full_cached=False):
-        self.full_cached = full_cached
+        self._num_nodes = core_nids.shape[0] + other_nids.shape[0]
 
-        start = time.time()
-        cache_size = 0
+        # to split nids into core & other
+        self._core_mask = torch.zeros((self._num_nodes, ), dtype=torch.bool)
+        self._core_mask[core_nids] = True
+        self._core_mask = self._core_mask.cuda()
 
-        if self.full_cached:
-            self.cached_feature = self.feature.cuda()
-            cache_size = self.cached_feature.numel(
-            ) * self.cached_feature.element_size()
+        # map nids to idx in core_feature or other_feature
+        self._idx_map = torch.argsort(torch.cat([core_nids, other_nids]))
+        self._idx_map[self._idx_map >=
+                      core_nids.shape[0]] -= core_nids.shape[0]
+        self._idx_map = self._idx_map.cuda()
 
-        else:
-            if cache_nids.numel() > 0:
-                self.cached_feature = self.feature[
-                    cache_nids.cpu().long()].cuda()
-                self.hashmap_key, self.hashmap_value = capi._CAPI_create_hashmap(
-                    cache_nids.cuda())
-                cache_size = self.cached_feature.numel(
-                ) * self.cached_feature.element_size()
+        self._cached_core_feature = None
+        self._cached_other_feature = None
 
-            else:
-                self.no_cached = True
-                cache_size = 0
+        self._core_hashmap_key = None
+        self._core_hashmap_value = None
+        self._core_full_cached = False
+        self._core_no_cached = False
 
-            capi._CAPI_pin_tensor(self.feature)
-
-        torch.cuda.synchronize()
-        end = time.time()
-
-        print(
-            "GPU {} takes {:.3f} s to cache feature data, cached size = {:.3f} GB, cache rate = {:.3f}"
-            .format(
-                torch.cuda.current_device(), end - start,
-                cache_size / 1024 / 1024 / 1024, cache_size /
-                (self.feature.element_size() * self.feature.numel())))
-
-    def fetch_data(self, index):
-        if self.full_cached:
-            return torch.index_select(self.cached_feature, 0, index.cuda())
-        elif self.no_cached:
-            return capi._CAPI_fetch_feature_data(self.feature, index)
-        else:
-            return capi._CAPI_fetch_feature_data_with_caching(
-                self.feature,
-                self.cached_feature,
-                self.hashmap_key,
-                self.hashmap_value,
-                index.cuda(),
-            )
+        self._other_hashmap_key = None
+        self._other_hashmap_value = None
+        self._other_full_cached = False
+        self._other_no_cached = False
 
     def __del__(self):
-        if not self.full_cached:
-            capi._CAPI_unpin_tensor(self.feature)
+        if not self._core_full_cached:
+            capi._CAPI_unpin_tensor(self._core_feature)
+        if not self._other_full_cached:
+            capi._CAPI_unpin_tensor(self._other_feature)
+
+    def cache_data(self, core_cache_idx, other_cache_idx):
+        tic = time.time()
+
+        if core_cache_idx.shape[0] == self._core_feature.shape[0]:
+            self._core_full_cached = True
+            self._cached_core_feature = self._core_feature.cuda()
+            core_mem_used = self._cached_core_feature.element_size(
+            ) * self._cached_core_feature.numel()
+        elif core_cache_idx.shape[0] == 0:
+            self._core_no_cached = True
+            capi._CAPI_pin_tensor(self._core_feature)
+            core_mem_used = 0
+        else:
+            self._core_hashmap_key, self._core_hashmap_value = capi._CAPI_create_hashmap(
+                core_cache_idx.cuda())
+            self._cached_core_feature = self._core_feature[
+                core_cache_idx].cuda()
+            capi._CAPI_pin_tensor(self._core_feature)
+            core_mem_used = self._cached_core_feature.element_size(
+            ) * self._cached_core_feature.numel(
+            ) + self._core_hashmap_key.element_size(
+            ) * self._core_hashmap_key.numel(
+            ) + self._core_hashmap_value.element_size(
+            ) * self._core_hashmap_value.numel()
+
+        if other_cache_idx.shape[0] == self._other_feature.shape[0]:
+            self._other_full_cached = True
+            self._cached_other_feature = self._other_feature.cuda()
+            other_mem_used = self._cached_other_feature.element_size(
+            ) * self._cached_other_feature.numel()
+        elif other_cache_idx.shape[0] == 0:
+            self._other_no_cached = True
+            capi._CAPI_pin_tensor(self._other_feature)
+            other_mem_used = 0
+        else:
+            self._other_hashmap_key, self._other_hashmap_value = capi._CAPI_create_hashmap(
+                other_cache_idx.cuda())
+            self._cached_other_feature = self._other_feature[
+                other_cache_idx].cuda()
+            capi._CAPI_pin_tensor(self._other_feature)
+            other_mem_used = self._cached_other_feature.element_size(
+            ) * self._cached_other_feature.numel(
+            ) + self._other_hashmap_key.element_size(
+            ) * self._other_hashmap_key.numel(
+            ) + self._other_hashmap_value.element_size(
+            ) * self._other_hashmap_value.numel()
+
+        toc = time.time()
+
+        info = "========================================\n"
+        info += "Rank {} builds feature cache in GPU\n".format(
+            torch.cuda.current_device())
+        info += "Core feature cache num {}, used mem {:.3f} GB\n".format(
+            core_cache_idx.shape[0], core_mem_used / 1024 / 1024 / 1024)
+        info += "Other feature cache num {}, used mem {:.3f} GB\n".format(
+            other_cache_idx.shape[0], other_mem_used / 1024 / 1024 / 1024)
+        info += "Build cache time {:.3f} ms\n".format((toc - tic) * 1000)
+        info += "========================================"
+        print(info)
+
+    def __getitem__(self, nids):
+        '''
+        nids is a gpu tensor
+        and return fetched_core_feature, fetched_other_feature, core_mask
+        '''
+        core_mask = self._core_mask[nids]
+
+        core_nids = nids[core_mask]
+        core_idx = self._idx_map[core_nids]
+        if self._core_full_cached:
+            fetched_core_features = self._cached_core_feature[core_idx]
+        elif self._core_no_cached:
+            fetched_core_features = capi._CAPI_fetch_feature_data(
+                self._core_feature, core_idx)
+        else:
+            fetched_core_features = capi._CAPI_fetch_feature_data_with_caching(
+                self._core_feature,
+                self._cached_core_feature,
+                self._core_hashmap_key,
+                self._core_hashmap_value,
+                core_idx,
+            )
+
+        other_nids = nids[~core_mask]
+        other_idx = self._idx_map[other_nids]
+        if self._other_full_cached:
+            fetched_other_features = self._cached_other_feature[other_idx]
+        elif self._other_no_cached:
+            fetched_other_features = capi._CAPI_fetch_feature_data(
+                self._other_feature, other_idx)
+        else:
+            fetched_other_features = capi._CAPI_fetch_feature_data_with_caching(
+                self._other_feature,
+                self._cached_other_feature,
+                self._other_hashmap_key,
+                self._other_hashmap_value,
+                other_idx,
+            )
+
+        return fetched_core_features, fetched_other_features, core_mask
