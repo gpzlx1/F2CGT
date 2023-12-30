ae8f60a69fde2254ea2f21919530f811f3618237
Jiawei Yi <552388389@qq.com>
2023-12-30 08:37:14 +0000

    add dist training scripts

--- python/bifeat/shm.py
+++ python/bifeat/shm.py
@@ -315,3 +315,172 @@ class ShmManager(object):
         print("finish loading {}...".format(self.dataset_name))
 
         return graph_tensors, meta_data
+
+    def load_compressed_dataset(self,
+                                with_feature=True,
+                                with_valid=True,
+                                with_test=True):
+        print("load {}...".format(self.dataset_name))
+
+        meta_data = torch.load(os.path.join(self.dataset_path, "metadata.pt"))
+        assert meta_data["dataset"] == self.dataset_name
+
+        if self._is_chief:
+            labels = torch.load(os.path.join(self.dataset_path, "labels.pt"))
+            shm_labels = self.create_shm_tensor(
+                self.dataset_name + "_shm_labels", labels.dtype, labels.shape)
+            shm_labels.copy_(labels)
+            del labels
+
+            indptr = torch.load(os.path.join(self.dataset_path, "indptr.pt"))
+            shm_indptr = self.create_shm_tensor(
+                self.dataset_name + "_shm_indptr", indptr.dtype, indptr.shape)
+            shm_indptr.copy_(indptr)
+            del indptr
+
+            indices = torch.load(os.path.join(self.dataset_path, "indices.pt"))
+            shm_indices = self.create_shm_tensor(
+                self.dataset_name + "_shm_indices", indices.dtype,
+                indices.shape)
+            shm_indices.copy_(indices)
+            del indices
+
+            train_idx = torch.load(
+                os.path.join(self.dataset_path, "train_idx.pt"))
+            shm_train_idx = self.create_shm_tensor(
+                self.dataset_name + "_shm_train_idx", train_idx.dtype,
+                train_idx.shape)
+            shm_train_idx.copy_(train_idx)
+            del train_idx
+
+            adj_hotness = torch.load(
+                os.path.join(self.dataset_path, "adj_hotness.pt"))
+            shm_adj_hotness = self.create_shm_tensor(
+                self.dataset_name + "_shm_adj_hotness", adj_hotness.dtype,
+                adj_hotness.shape)
+            shm_adj_hotness.copy_(adj_hotness)
+            del adj_hotness
+
+            feat_hotness = torch.load(
+                os.path.join(self.dataset_path, "feat_hotness.pt"))
+            shm_feat_hotness = self.create_shm_tensor(
+                self.dataset_name + "_shm_feat_hotness", feat_hotness.dtype,
+                feat_hotness.shape)
+            shm_feat_hotness.copy_(feat_hotness)
+            del feat_hotness
+
+            codebooks = torch.load(
+                os.path.join(self.dataset_path, "codebooks.pt"))
+            shm_codebooks = self.create_shm_tensor(
+                self.dataset_name + "_shm_codebooks", codebooks.dtype,
+                codebooks.shape)
+            shm_codebooks.copy_(codebooks)
+            del codebooks
+
+            core_codebooks = torch.load(
+                os.path.join(self.dataset_path, "core_codebooks.pt"))
+            shm_core_codebooks = self.create_shm_tensor(
+                self.dataset_name + "_shm_core_codebooks",
+                core_codebooks.dtype, core_codebooks.shape)
+            shm_core_codebooks.copy_(core_codebooks)
+            del core_codebooks
+
+            if with_feature:
+                features = torch.load(
+                    os.path.join(self.dataset_path, "compressed_features.pt"))
+                core_features = torch.load(
+                    os.path.join(self.dataset_path,
+                                 "compressed_core_features.pt"))
+                shm_features = self.create_shm_tensor(
+                    self.dataset_name + "_shm_features", features.dtype,
+                    features.shape)
+                shm_features.copy_(features)
+                del features
+                shm_core_features = self.create_shm_tensor(
+                    self.dataset_name + "_shm_core_features",
+                    core_features.dtype, core_features.shape)
+                shm_core_features.copy_(core_features)
+                del core_features
+
+            if with_valid:
+                valid_idx = torch.load(
+                    os.path.join(self.dataset_path, "valid_idx.pt"))
+                shm_valid_idx = self.create_shm_tensor(
+                    self.dataset_name + "_shm_valid_idx", valid_idx.dtype,
+                    valid_idx.shape)
+                shm_valid_idx.copy_(valid_idx)
+                del valid_idx
+
+            if with_test:
+                test_idx = torch.load(
+                    os.path.join(self.dataset_path, "test_idx.pt"))
+                shm_test_idx = self.create_shm_tensor(
+                    self.dataset_name + "_shm_test_idx", test_idx.dtype,
+                    test_idx.shape)
+                shm_test_idx.copy_(test_idx)
+                del test_idx
+
+            if not (with_valid or with_test):
+                core_idx = train_idx
+            else:
+                core_idx = torch.load(
+                    os.path.join(self.dataset_path, "core_idx.pt"))
+            shm_core_idx = self.create_shm_tensor(
+                self.dataset_name + "_shm_core_idx", core_idx.dtype,
+                core_idx.shape)
+            shm_core_idx.copy_(core_idx)
+            del core_idx
+
+        else:
+            shm_labels = self.create_shm_tensor(
+                self.dataset_name + "_shm_labels", None, None)
+            shm_indptr = self.create_shm_tensor(
+                self.dataset_name + "_shm_indptr", None, None)
+            shm_indices = self.create_shm_tensor(
+                self.dataset_name + "_shm_indices", None, None)
+            shm_train_idx = self.create_shm_tensor(
+                self.dataset_name + "_shm_train_idx", None, None)
+            shm_adj_hotness = self.create_shm_tensor(
+                self.dataset_name + "_shm_adj_hotness", None, None)
+            shm_feat_hotness = self.create_shm_tensor(
+                self.dataset_name + "_shm_feat_hotness", None, None)
+            shm_codebooks = self.create_shm_tensor(
+                self.dataset_name + "_shm_codebooks", None, None)
+            shm_core_codebooks = self.create_shm_tensor(
+                self.dataset_name + "_shm_core_codebooks", None, None)
+            if with_feature:
+                shm_features = self.create_shm_tensor(
+                    self.dataset_name + "_shm_features", None, None)
+                shm_core_features = self.create_shm_tensor(
+                    self.dataset_name + "_shm_core_features", None, None)
+            if with_valid:
+                shm_valid_idx = self.create_shm_tensor(
+                    self.dataset_name + "_shm_valid_idx", None, None)
+            if with_test:
+                shm_test_idx = self.create_shm_tensor(
+                    self.dataset_name + "_shm_test_idx", None, None)
+            shm_core_idx = self.create_shm_tensor(
+                self.dataset_name + "_shm_core_idx", None, None)
+
+        dist.barrier()
+
+        graph_tensors = {
+            "labels": shm_labels,
+            "indptr": shm_indptr,
+            "indices": shm_indices,
+            "train_idx": shm_train_idx,
+            "adj_hotness": shm_adj_hotness,
+            "feat_hotness": shm_feat_hotness,
+            "core_idx": shm_core_idx
+        }
+        if with_feature:
+            graph_tensors["features"] = shm_features
+            graph_tensors["core_features"] = shm_core_features
+        if with_valid:
+            graph_tensors["valid_idx"] = shm_valid_idx
+        if with_test:
+            graph_tensors["test_idx"] = shm_test_idx
+
+        print("finish loading {}...".format(self.dataset_name))
+
+        return graph_tensors, meta_data, [shm_core_codebooks, shm_codebooks]
